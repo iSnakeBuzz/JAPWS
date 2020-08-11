21bd6bb0827b493fe69973d3286ea3607e0053d8
iSnakeBuzz <contacto@isnakebuzz.com>
2020-08-11 18:53:34 -0300

    Added products.html and login.html

--- js/login.js
+++ js/login.js
@@ -1,6 +1,50 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
+
+    const data = {
+        username: "",
+        password: "",
+    };
+
+    let userInput = document.getElementById('userInput');
+    let passInput = document.getElementById('passInput');
+    let send = document.getElementById('send');
+
+    userInput.addEventListener('change', (e) => {
+        console.log(e)
+        data.username = e.target.value;
+    });
+
+    passInput.addEventListener('change', (e) => {
+        console.log(e)
+        data.password = e.target.value;
+    })
+
+    send.addEventListener('click', (e) => {
+        let alert = document.getElementById('alert');
+        alert.classList.remove("bg-success")
+        alert.classList.remove("bg-danger")
+
+
+        if (data.username.length > 0 && data.password.length > 0) {
+            alert.classList.add("bg-success")
+            alert.classList.remove("d-none")
+            alert.innerText = "Has ingresado con exito! Redirigiendo...";
+
+            localStorage.setItem("logged", "esto es un estado xD");
+            setTimeout(() => {
+                window.location.href = "index.html";
+            }, 1000 * 3)
+        } else {
+            alert.classList.add("bg-danger")
+            alert.classList.remove("d-none")
+            alert.innerText = "Por favor introduce contenido en todos los campos para poder continuar!";
+        }
+
+    })
+
+    console.log("Hello? XD")
 });
--- js/products.js
+++ js/products.js
@@ -2,5 +2,30 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(resp => {
+        console.log(resp)
+        let doc = document.getElementById('products-list');
 
+        resp.data.forEach(data => {
+            console.log(data)
+            doc.innerHTML += template(data.name, data.description, data.imgSrc, data.currency + " " + data.cost);
+        })
+    })
 });
+
+function template(name, desc, img, price) {
+    return (`
+        <div class="col col-auto">
+            <div class="card" style="width: 20rem; ">
+                <img src=${img} class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${name}<label class="float-right" style="font-size: 14px">${price}</label></h5>
+                        <p class="card-text">
+                        ${desc}
+                        </p>
+                        <!--<a href="#" class="btn btn-primary">Ver producto</a>-->
+                    </div>
+            </div>
+        </div>
+    `);
+}
